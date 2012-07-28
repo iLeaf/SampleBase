878766bd65c0c58672eeb0e35b0d932217249c20
iLeaf <studio_ileaf@yahoo.co.jp>
2012-07-28 17:42:56 +0900

    first commit

---
+++ SampleBase/studio_iLeafViewController.h
@@ -0,0 +1,13 @@
+//
+//  studio_iLeafViewController.h
+//  SampleBase
+//
+//  Created by 平杉 敦史 on 12/07/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface studio_iLeafViewController : UIViewController
+
+@end
