99142c5a56c4f954ffdd4fbc74718641cb4983f5
iLeaf <studio_ileaf@yahoo.co.jp>
2012-07-29 23:24:20 +0900

    オートリサイズさせない

---
+++ SampleBase/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  SampleBase
+//
+//  Created by 平杉 敦史 on 12/07/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ SampleBase/SearchViewController.m
@@ -0,0 +1,43 @@
+//
+//  SearchViewController.m
+//  SampleBase
+//
+//  Created by 平杉 敦史 on 12/07/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
